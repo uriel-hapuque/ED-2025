e21d4bdebcae3847510334b6266f80ecfa1ab3de
Uriel Hapuque Barboza <109743776+uriel-hapuque@users.noreply.github.com>
2025-04-03 10:38:56 -0300

    Add files via upload

--- concat.c
+++ concat.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 
+int concat(int *arr1, int *arr2, int tamanho_arr1, int tamanho_arr2, int *arr3);
+
 int main(){
     int arr1[] = {1, 2, 3, 4, 5};
     int arr2[] = {6, 7, 8, 9, 10};
     int tamanho_arr1 = sizeof(arr1)/sizeof(arr1[0]);
     int tamanho_arr2 = sizeof(arr2)/sizeof(arr2[0]);
-    
+    int tamanho_arr3 = tamanho_arr1 + tamanho_arr2;
+    int arr3[tamanho_arr3];
+    int i;
+
+    concat(arr1, arr2, tamanho_arr1, tamanho_arr2, arr3);
+
+    for(i = 0; i < tamanho_arr3; i++){
+        printf("%d ", arr3[i]);
+    }
     return 0;
 }
 
-int concat(int *arr1, int *arr2, int tamanho_arr1, int tamanho_arr2){
-    int i, j;
+int concat(int *arr1, int *arr2, int tamanho_arr1, int tamanho_arr2, int *arr3){
+    int i;
+    for(i = 0; i < tamanho_arr1; i++){
+        arr3[i] = arr1[i];
+    }
+
+    for(i = 0; i < tamanho_arr2; i++){
+        arr3[tamanho_arr1 + i] = arr2[i];
+    }
+
     return 0;
 }
--- soma_rec.c
+++ soma_rec.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
 
-int soma_rec(int *arr, int tamanho);
+// int soma_rec(int *arr, int tamanho);
 
 int main(){
-    int arr[] = {1, 2, 3, 4, 5};
-    int tamanho_arr = sizeof(arr)/sizeof(arr[0]);
+    // int arr[] = {1, 2, 3, 4, 5};
+    // int tamanho_arr = sizeof(arr)/sizeof(arr[0]);
 
-    printf("%d", soma_rec(arr, tamanho_arr));
+    // printf("%d", soma_rec(arr, tamanho_arr));
+    printf("%d", soma(5));
     return 0;
 }
 
-int soma_rec(int *arr, int tamanho){
-    if(tamanho == 0) return 0;
-    return arr[tamanho-1] + soma_rec(arr, tamanho-1);
+// int soma_rec(int *arr, int tamanho){
+//     if(tamanho == 0) return 0;
+//     return arr[tamanho-1] + soma_rec(arr, tamanho-1);
+// }
+
+int soma(int const n)
+{
+
+if (n == 0)
+return 0;
+else
+return n + soma(n - 1); // adicionar 
+
 }
